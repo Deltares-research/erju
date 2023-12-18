6ea6e26727e0bc51b3783cb01465a18ea7ad0a9b
camposmo <fabian.camposmontero@deltares.nl>
2023-12-18 16:11:41 +0100

    Added Silixa iDAS TDMS reader to utils

--- erju/test_run.py
+++ erju/test_run.py
@@ -11,7 +11,7 @@ dir_path = r'D:\FO_culemborg_22112020\subtest'
 first_channel = 0
 last_channel = 500
 
-
+#########################################################################
 # Start the timer
 start_time = time.time()
 
@@ -32,38 +32,10 @@ selected_files = train_22_cul.get_files_above_threshold(signal_mean, threshold=5
 # From the selected files, extract the data
 all_data = train_22_cul.get_data(selected_files)
 
-
+#################################################################################
 # Plot the data
 # Create the plotting instance
 train_22_cul_plots = PlotData(dir_path, selected_files[0], all_data)
 train_22_cul_plots.plot_data(save_figure=True)
 
 
-
-##################
-# OLD TEST
-
-#
-# # Initialize the ReadTDMS class instance
-# read_tdms = ReadTDMS(file_path, first_channel, last_channel)
-# # Get the properties of the TDMS file
-# properties = read_tdms.get_properties()
-# # Get the selected data from the TDMS file
-# data = read_tdms.get_data()
-#
-# # Initialize the ScanForTrains class instance with the ReadTDMS instance
-# scan_for_trains = ScanForTrains(data, properties, first_channel, last_channel)
-#
-# # Grab the search parameters
-# scan_channel, start_time, end_time = scan_for_trains.search_params()
-#
-# print('scan_channel: ', scan_channel)
-# print('start_time: ', start_time)
-# print('end_time: ', end_time)
-#
-# # Get the average signal
-# mean_signal = scan_for_trains.signal_averaging()
-# print('mean_signal: ', mean_signal)
-#
-# # Plot the selected data
-# #read_tdms.plot_data(save_figure=True)
