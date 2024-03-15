0792825afcce0052a97d51127fd7bb53861cec43
DeltaresProRailDAS <62556956+Edwin170281@users.noreply.github.com>
2024-03-15 12:56:54 +0100

    New functions in utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,7 @@
+import h5py
+import obspy
+from datetime import datetime
+
 def get_file_names(self):
     """
     Get a list of unique file names inside the dir_path folder. For this
@@ -16,3 +20,42 @@ def get_file_names(self):
     self.file_names = [f.split('.')[0] for f in ascii_file_names]
 
     return self.file_names
+
+
+
+
+#def filterSignal():
+
+
+
+
+
+def readOptaSenseProps(fpath):
+
+    # Open the file for reading
+    fp = h5py.File(fpath,'r')
+
+    # Print the attributes for "Acquisition"
+    print(fp['Acquisition'].attrs.keys())
+
+    # Print the values associated with some important attributes
+    print('\nThe gauge length is ',fp['Acquisition'].attrs['GaugeLength'],'m.')
+    print('The pulse rate is ',fp['Acquisition'].attrs['PulseRate'],'Hz.')
+    print('The spatial sampling interval is ',fp['Acquisition'].attrs['SpatialSamplingInterval'],'m.')
+
+
+    # Create a new variable for the "RawDataTime" h5py dataset so we don't have to type so much
+    rawDataTime = fp['Acquisition']['Raw[0]']['RawDataTime']
+
+    # We can load the full contents of "RawDataTime" into a NumPy array
+    rawDataTimeArr = rawDataTime[:]
+
+    # Print the length of the array
+    print('\nThere are',len(rawDataTimeArr),'values in \"RawDataTime\"')
+
+    # Print the beginning and ending time of the dataset
+    print('The first sample is at',datetime.utcfromtimestamp(rawDataTimeArr[0]*1e-6),\
+        'and the last sample is at',datetime.utcfromtimestamp(rawDataTimeArr[-1]*1e-6))
+
+
+
