7d35a78076e1f79c460cdf675878439e0715f7ad
camposmo <fabian.camposmontero@deltares.nl>
2024-06-12 17:39:47 +0200

    changed my manual test for a unittest in the create windows

--- test/test_create_windows.py
+++ test/test_create_windows.py
@@ -1,55 +1,58 @@
+import unittest
 from erju.Accel.create_data_windows import AccelDataTimeWindows
 from utils.utils import get_files_in_dir
+import pandas as pd
 
 
-# Define the path to the folder containing the accelerometer data
-accel_data_path = r'D:\accel_trans\culemborg_data'
-window_size_extension = 10  # seconds
-event_separation_internal = 5  # seconds
-threshold = 0.02
-trigger_on = 7
-trigger_off = 1
+class TestAccelAnalysis(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Set up the test environment
+        cls.accel_data_path = r'D:\accel_trans\culemborg_data'
+        cls.window_size_extension = 10  # seconds
+        cls.event_separation_internal = 5  # seconds
+        cls.threshold = 0.02
+        cls.trigger_on = 7
+        cls.trigger_off = 1
 
-# Create an instance of the AccelDataTimeWindows class
-time_windows = AccelDataTimeWindows(accel_data_path=accel_data_path,
-                                    window_buffer=window_size_extension,
-                                    event_separation_internal=event_separation_internal,
-                                    threshold=threshold)
+        # Create an instance of AccelDataTimeWindows
+        cls.time_windows = AccelDataTimeWindows(accel_data_path=cls.accel_data_path,
+                                                window_buffer=cls.window_size_extension,
+                                                event_separation_internal=cls.event_separation_internal,
+                                                threshold=cls.threshold)
+        # Get the list of file names in the directory
+        cls.file_names = get_files_in_dir(folder_path=cls.accel_data_path, file_format='.asc', keep_extension=False)
 
-# Get the list of file names in the folder
-file_names = get_files_in_dir(folder_path=accel_data_path, file_format='.asc', keep_extension=False)
+    def test_accel_analysis(self):
+        # Test the functionality of the accelerometer analysis
 
-# Create a dataframe with the data from the first location, specifying the number of columns
-# (in this case 3, because we use the first 3 columns of data from the file) and get the data
-# from the first file in the list
-accel_data_df = time_windows.extract_accel_data_from_file(file_names[0], no_cols=3)
+        # Load accelerometer data
+        accel_data_df = self.time_windows.extract_accel_data_from_file(self.file_names[0], no_cols=3)
 
-# Find the indices and times where the combined signal crosses the threshold
-windows_indices, windows_times = time_windows.create_windows_indices_and_times(accel_data_df)
+        # Check if the loaded data is a DataFrame with 3 columns
+        self.assertIsInstance(accel_data_df, pd.DataFrame)
+        self.assertEqual(accel_data_df.shape[1], 3)
 
-# Detect events using STA/LTA method
-nsta = int(0.5 * 1000)  # 0.5 seconds window for STA
-nlta = int(5 * 1000)  # 10 seconds window for LTA
-windows_indices_sta_lta, windows_times_sta_lta = time_windows.detect_events_with_sta_lta(accel_data_df,
-                                                                                         nsta,
-                                                                                         nlta,
-                                                                                         trigger_on=trigger_on,
-                                                                                         trigger_off=trigger_off)
-# Print the results with both methods
-print('For the old method:')
-print('number of windows:', len(windows_indices))
-print('windows indices:', windows_indices)
-print('windows times:', windows_times)
-print('For the STA/LTA method:')
-print('number of windows:', len(windows_indices_sta_lta))
-print('windows indices:', windows_indices_sta_lta)
-print('windows times:', windows_times_sta_lta)
+        # Create window indices and times using the threshold method
+        windows_indices, windows_times = self.time_windows.create_windows_indices_and_times(accel_data_df)
+
+        # Check if windows are created
+        self.assertGreater(len(windows_indices), 0)
+        self.assertGreater(len(windows_times), 0)
+
+        # Detect events using the STA/LTA method
+        nsta = int(0.5 * 1000)  # 0.5 seconds window for STA
+        nlta = int(5 * 1000)  # 5 seconds window for LTA
+        windows_indices_sta_lta, windows_times_sta_lta = self.time_windows.detect_events_with_sta_lta(accel_data_df,
+                                                                                                      nsta, nlta,
+                                                                                                      self.trigger_on,
+                                                                                                      self.trigger_off)
+
+        # Check if events are detected
+        self.assertGreater(len(windows_indices_sta_lta), 0)
+        self.assertGreater(len(windows_times_sta_lta), 0)
 
 
-# Plot the signal and STA/LTA ratio with detected events using threshold method
-time_windows.plot_accel_signal_and_windows(accel_data_df, windows_indices)
-
-# Plot the signal and STA/LTA ratio with detected events using STA/LTA method
-time_windows.plot_accel_signal_and_windows(accel_data_df, windows_indices_sta_lta,
-                                           nsta, nlta, trigger_on=trigger_on, trigger_off=trigger_off)
-
+if __name__ == '__main__':
+    # Run the unittests
+    unittest.main()
