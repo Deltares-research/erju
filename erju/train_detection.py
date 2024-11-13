0b2c0c37c1e33b21c521d3f187419924e2e59cc1
camposmo <fabian.camposmontero@deltares.nl>
2024-11-13 16:47:27 +0100

    New utils file just for the .h5 type of file.

--- erju/train_detection.py
+++ erju/train_detection.py
@@ -7,6 +7,7 @@
 import h5py
 from loguru import logger
 from utils.file_utils import get_files_list, get_file_extensions
+from utils.h5_utils import calculate_sampling_frequency
 
 def detect_trainpassage_single_channel_h5(folder_path: str):
     """
@@ -20,8 +21,20 @@ def detect_trainpassage_single_channel_h5(folder_path: str):
     file_extension = '*.h5'
     file_paths = get_files_list(folder_path=folder_path, file_extension=file_extension)
 
+    # Get metadata from the first file
+    # Compare with the last file to ensure they are the same
+    with h5py.File(file_paths[0], "r") as file_start, h5py.File(file_paths[-1], "r") as file_end:
+        sf = calculate_sampling_frequency(file_start)
+        if sf != calculate_sampling_frequency(file_end):
+            raise ValueError("Sampling frequency is not the same in the begin and end files")
 
-def detect_trainpassage_single_channel(folder_path: str):
+        print(f"Sampling frequency: {sf} Hz")
+
+
+
+
+
+def detect_trainpassage_single_channel(folder_path: str, channel: int):
     """
     Detect the train passage in a single channel. This process looks into a specific channel (location), file by file
     for a signal anomaly that indicates the passage of a train. It will call a different function depending on the
@@ -29,6 +42,7 @@ def detect_trainpassage_single_channel(folder_path: str):
 
      Args:
             folder_path (str): The path to the directory containing the files.
+            channel (int): The channel number to look for the train passage.
     """
 
     # Browse the directory and gather a list of all the file extensions.
@@ -58,7 +72,7 @@ def detect_trainpassage_single_channel(folder_path: str):
 ########################################################################################################################
 
 files_path = r'C:\Projects\erju\data\holten\recording_2024-08-29T08_01_16Z_5kHzping_1kHzlog_1mCS_10mGL_3000channels'
-detect_trainpassage_single_channel(folder_path=files_path)
+detect_trainpassage_single_channel(folder_path=files_path, channel=1200)
 
-# I will now test the function with the Silixa files
+
 print("The function has been tested successfully!")
---
+++ utils/h5_utils.py
@@ -0,0 +1,35 @@
+import h5py
+
+
+
+def calculate_sampling_frequency(file: h5py.File) -> float:
+    """
+    Calculate the sampling frequency from an open HDF5 file by measuring the time interval
+    between consecutive samples in the 'RawDataTime' dataset.
+
+    Args:
+        file (h5py.File): An open HDF5 file object.
+
+    Returns:
+        float: The calculated sampling frequency in Hz.
+    """
+    try:
+        # Access the 'RawDataTime' dataset to get timestamps for calculating sampling interval
+        raw_data_time = file['Acquisition']['Raw[0]']['RawDataTime']
+
+        # Calculate time interval between the first two samples in seconds
+        time_interval = (raw_data_time[1] - raw_data_time[0]) * 1e-6  # Convert from microseconds to seconds
+
+        # Sampling frequency is the inverse of the time interval
+        sampling_frequency = 1 / time_interval
+        # Make sampling frequency an integer
+        sampling_frequency = int(sampling_frequency)
+
+        return sampling_frequency
+
+    except KeyError:
+        raise ValueError("The 'RawDataTime' dataset is missing in the file structure.")
+    except IndexError:
+        raise ValueError("The 'RawDataTime' dataset has insufficient data for frequency calculation.")
+
+
